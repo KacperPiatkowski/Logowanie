9211e8526304f9b96d090b1ccf493e012b2d6bc0
KacperPiatkowski <k.piatkowski07@gmail.com>
2024-02-05 12:21:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/logowanie/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.logowanie
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tablica =intent.getStringArrayExtra("Logowanie")
+        var zalogowano = false
+        var tab = arrayOf("", "")
+        if (tablica != null){
+            zalogowano = true
+            tab = tablica
+        }
+
+        findViewById<Button>(R.id.LogIn).setOnClickListener {
+            val email = findViewById<EditText>(R.id.Email).text.toString()
+            val password = findViewById<EditText>(R.id.Password).text.toString()
+
+            if (email.isEmpty() || password.isEmpty())
+                Toast.makeText(this, "Fill in all fields", Toast.LENGTH_SHORT).show()
+            else if (email != "k.piatkowski07@gmail.com" || password != "kacper11")
+                Toast.makeText(this, "Login error, incorrect email or password.", Toast.LENGTH_SHORT).show()
+            else {
+                zalogowano = true
+                tab[0] = email
+                tab[1] = password
+
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+
+                startActivity(Intent(this, IndexActivity::class.java).putExtra("Logowanie", tab));
+
+            }
+        }
+
+    }
+}
